bce0b035a5b111913b1edad386402bcf8cd5f0b8
MohamadMahdy Raeisi <66519118+RedDeadGhost@users.noreply.github.com>
2022-12-12 01:47:42 +0330

    Project 02 was added
    A simple time date time showing in the template using javascript and JS DOM.

---
+++ Project_02/assets/scripts/JS_Pr02_Script.js
@@ -0,0 +1,5 @@
+
+const rightNow = new Date();
+const myTemplate_1 = `<p id="show-time">Right Now Time is: ${rightNow} | ${rightNow.toDateString()}</p>`;
+let send_time_to_element = document.querySelector(".second-div")
+send_time_to_element.innerHTML = myTemplate_1
