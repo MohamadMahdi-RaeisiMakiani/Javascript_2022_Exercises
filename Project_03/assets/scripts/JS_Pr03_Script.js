686265fed7eebf732b20ec8a016c03150e50e846
MohamadMahdy Raeisi <66519118+RedDeadGhost@users.noreply.github.com>
2022-12-13 23:46:36 +0330

    Javascript code 3 was fixed
    I fix the code 3

--- Project_03/assets/scripts/JS_Pr03_Script.js
+++ Project_03/assets/scripts/JS_Pr03_Script.js
@@ -1,7 +1,18 @@
 
 const rightNow = new Date();
 const myTemplate_1 = `<p id="show-time">Right Now Time is: ${rightNow} | ${rightNow.toDateString()}</p>`;
-let send_time_to_element = document.querySelector(".second-div")
-send_time_to_element.innerHTML = myTemplate_1
+let send_time_to_element = document.querySelector(".second-div");
+send_time_to_element.innerHTML = myTemplate_1;
 
-let add_p_element_1
+let add_p_element_1 = document.createElement("p");
+add_p_element_1.setAttribute("id", "test-para-1");
+add_p_element_1.setAttribute("style", "width: 75rem;");
+add_p_element_1.innerText = `This is a test Paragraph, it was added to this html page from a javascript code.`;
+add_p_element_1.style["text-align"] = "center";
+add_p_element_1.style.color = "green";
+add_p_element_1.style.border = "1px solid black";
+add_p_element_1.style.margin = "1rem auto";
+send_time_to_element.appendChild(add_p_element_1);
+
+select_p_element_1 = document.getElementById("test-para-1")
+select_p_element_1.style["font-size"] = "1.5rem"
